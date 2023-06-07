d0f585f65f5eb90d6168657a642c06d0c6497dfd
Denwiliusz27 <danwielgosz@gmail.com>
2023-06-07 11:39:49 +0200

    project init

---
+++ PROJEKT/ShoppingList/ShoppingList/ViewModels/HomeViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ShoppingList.ViewModels
+{
+    public class HomeViewModel
+    {
+        public List<string> products { get; set; } = new List<string>();
+
+        public string newProductName { get; set; }
+
+        private void addNewProduct() {
+            var newProduct = newProductName;
+
+            products.Add(newProduct);
+        }
+    }
+}
