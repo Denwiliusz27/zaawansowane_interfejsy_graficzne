a1fcddb137518cc197e926b7b8fc01ff245d443f
Denwiliusz27 <danwielgosz@gmail.com>
2023-06-10 15:17:37 +0200

    cleaned code

--- ProductsApp/ProductsApp/Database/ProductsDbContext.cs
+++ ProductsApp/ProductsApp/Database/ProductsDbContext.cs
@@ -13,21 +13,12 @@ namespace ProductsApp.Database
     public class ProductsDbContext : DbContext
     {
         public DbSet<ProductModel> Products { get; set; }
+        public DbSet<BasketProduct> BasketProducts { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             string path = Path.Combine(Environment.CurrentDirectory, "Tasks.db");
             optionsBuilder.UseSqlite($"Filename={path}");
         }
-
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //{
-        //    modelBuilder.Entity<ProductModel>()
-        //        .HasData(
-        //            new ProductModel { Id = 1, Name = "jabłka", Value = 15.4 },
-        //            new ProductModel { Id = 2, Name = "mleko", Value = 4.45 },
-        //            new ProductModel { Id = 3, Name = "woda", Value = 1.99 });
-        //}
-
     }
 }
---
+++ ProductsApp/ProductsApp/Models/BasketProduct.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductsApp.Models
+{
+    public class BasketProduct
+    {
+        public int Id { get; set; }
+        public int Amount { get; set; }
+
+        [ForeignKey("ProductId")]
+        public ProductModel product { get; set; }
+    }
+}
